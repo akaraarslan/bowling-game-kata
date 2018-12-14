740f130bc7e3a78c53a8115b084abc0134feb268
darklion <akrrsln@gmail.com>
2018-12-14 11:54:26 +0300

    added 2 new tests
    config

--- bowling-game-kata.test/GameTest.cs
+++ bowling-game-kata.test/GameTest.cs
@@ -5,16 +5,41 @@ namespace bowling_game_kata.test
     public class GameTest
     {
         [Fact]
-        public void Should_calculate20_when_allRolls1()
-        { 
+        public void Should_Calculate20_When_AllRolls1()
+        {
             Game game = new Game();
-             
+
             for (int i = 0; i < 20; i++)
             {
-                game.Roll(1);    
+                game.Roll(1);
             }
 
-          Assert.True(game.TotalScore == 20);   
-        } 
+            Assert.True(game.TotalScore == 20);
+        }
+
+        [Fact]
+        public void Should_Calculate80_When_AllRolls4()
+        {
+            Game game = new Game();
+
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(4);
+            }
+
+            Assert.True(game.TotalScore == 80);
+        }
+
+        [Fact]
+        public void Should_CalculateSpareBonus_When_Rolled3Times()
+        {
+            Game game = new Game();
+
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(4);
+
+            Assert.Equal(18, game.TotalScore);
+        }
     }
 }
--- bowling-game-kata/Game.cs
+++ bowling-game-kata/Game.cs
@@ -2,11 +2,22 @@
 {
     public class Game
     {
-        public int TotalScore { get; set; } 
+        private int[] RollKnockedPinsCountList = new int[21];
+        private int RollIndex { get; set; }
+        public int TotalScore { get; set; }
 
         public void Roll(int knockedPins)
         {
+            RollKnockedPinsCountList[RollIndex] = knockedPins;
+
             TotalScore += knockedPins;
+
+            if (RollIndex >= 2 && (RollKnockedPinsCountList[RollIndex - 1] + RollKnockedPinsCountList[RollIndex - 2]) == 10)
+            {
+                TotalScore += RollKnockedPinsCountList[RollIndex];
+            }
+
+            RollIndex++;
         }
     }
 }
