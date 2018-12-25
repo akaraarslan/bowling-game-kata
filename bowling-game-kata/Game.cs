2a578c9fd08f71115e0d4b221c3a5a18e8b218a8
darklion <akrrsln@gmail.com>
2018-12-25 10:42:34 +0300

    refactoring

--- bowling-game-kata/Game.cs
+++ bowling-game-kata/Game.cs
@@ -4,24 +4,34 @@
     {
         private int[] RollKnockedPinsCountList = new int[21];
         private int RollIndex { get; set; }
-        public int TotalScore { get; set; }
+        public int TotalScore
+        {
+            get
+            {
+                var result = 0;
+                for (int i = 0; i < RollKnockedPinsCountList.Length; i++)
+                {
+                    result += RollKnockedPinsCountList[i];
+
+                    if (i >= 2 && (RollKnockedPinsCountList[i - 1] + RollKnockedPinsCountList[i - 2]) == 10)
+                    {
+                        result += RollKnockedPinsCountList[i];
+                    }
+
+                    if (i >= 2 && RollKnockedPinsCountList[i - 2] == 10)
+                    {
+                        result += RollKnockedPinsCountList[i] + RollKnockedPinsCountList[i - 1];
+                    }
+
+                }
+                return result;
+            }
+            set { }
+        }
 
         public void Roll(int knockedPins)
         {
             RollKnockedPinsCountList[RollIndex] = knockedPins;
-
-            TotalScore += knockedPins;
-
-            if (RollIndex >= 2 && (RollKnockedPinsCountList[RollIndex - 1] + RollKnockedPinsCountList[RollIndex - 2]) == 10)
-            {
-                TotalScore += RollKnockedPinsCountList[RollIndex];
-            }
-
-              if (RollIndex >= 2 && (RollKnockedPinsCountList[RollIndex - 2]) == 10)
-            {
-                TotalScore += RollKnockedPinsCountList[RollIndex-1] + RollKnockedPinsCountList[RollIndex];
-            }
-
             RollIndex++;
         }
     }
